59cf36e202336daa11a9205730d0b088c129cfdd
MichaelMkr <it191501@fhstp.ac.at>
2020-01-08 10:30:53 +0100

    added possibility to toggle menu

--- public/javascript/functions.js
+++ public/javascript/functions.js
@@ -5,3 +5,13 @@ function test() {
 function debug(param1) {
     console.log(param1);
 }
+
+function showMenu() {
+    var x = document.getElementById("menu");
+    x.style.display = "block";
+}
+
+function hideMenu() {
+    var x = document.getElementById("menu");
+    x.style.display = "none";
+}
